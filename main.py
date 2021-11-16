f3e3f3ec82aa0f54231e2572a248095fb0e7a54d
Giotje_ <devgio@icloud.com>
2021-11-16 19:14:16 +0100

    added fizzbuzz

---
+++ main.py
@@ -0,0 +1,8 @@
+for fizzbuzz in range(51):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("fizzbuzz")
+    elif fizzbuzz % 3:
+        print("fizz")
+    elif fizzbuzz % 5:
+        print('buzz')
+    print(fizzbuzz)
